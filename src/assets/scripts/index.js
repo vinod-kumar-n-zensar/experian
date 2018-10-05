9e5a3777e564a77eb8fbdcc2e12ca21b4e325bbe
s.jambotkar@zensar.com <s.jambotkar@zensar.com>
2018-10-05 15:22:38 +0530

    header changes

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,8 +1,11 @@
 import 'roboto-fontface';
 import 'material-icons';
 import '../styles/app.scss';
+import '../styles/style.css';
 import '../styles/eds-global-styles.scss';
 import '../styles/header.scss';
+import '../styles/search.scss';
+import '../styles/caseList.scss';
 import '../styles/pcc-eds-secondary-header.scss';
 import '../styles/demo-pcc-overview.scss';
 import '../styles/pcc-accordian.scss';
@@ -11,5 +14,3 @@ import '../styles/eds-dropdown.scss';
 import '../styles/eds-icon.scss';
 import '../styles/eds-timeline-item.scss'
 import '../styles/eds-tag.scss'
-
-
