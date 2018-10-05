979316053bfe738940fa4277a58c277bc1b9f8df
Vinod Kumar <vinod.kumar1@zensar.com>
2018-10-05 14:22:20 +0530

    Account Summary

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,15 @@
 import 'roboto-fontface';
 import 'material-icons';
 import '../styles/app.scss';
+import '../styles/eds-global-styles.scss';
 import '../styles/header.scss';
+import '../styles/pcc-eds-secondary-header.scss';
+import '../styles/demo-pcc-overview.scss';
+import '../styles/pcc-accordian.scss';
+import '../styles/eds-card.scss';
+import '../styles/eds-dropdown.scss';
+import '../styles/eds-icon.scss';
+import '../styles/eds-timeline-item.scss'
+import '../styles/eds-tag.scss'
+
 
