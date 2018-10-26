16fcfbb50e4ea06dc4b37990a6b20036cd35c3bc
Vinod Kumar <vinod.kumar1@zensar.com>
2018-10-26 17:02:08 +0530

    IE issues solved

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,17 @@ module.exports = {
                     outputPath: 'fonts/'
                 }
             }]
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+            }
+          }
         }
       ]
   },
